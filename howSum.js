c58506f6340be8494393895ee579eb332310b7ae
marovichn <nikolamarovic.11@gmail.com>
2023-04-04 22:57:20 +0200

    howSum-example-2

--- howSum.js
+++ howSum.js
@@ -2,8 +2,15 @@ const howSum = (targetSum, numbers, memo = {}) => {
   if (targetSum === 0) return [];
   if (targetSum < 0) return null;
 
-  for (let num in numbers) {
+  for (let num of numbers) {
     const remainder = targetSum - num;
-    howSum(remainder, numbers);
+    const remainderResult = howSum(remainder, numbers);
+    if (remainderResult !== null) {
+      return [...remainderResult, num];
+    }
   }
+
+  return null;
 };
+
+console.log(howSum(0, [7]));
